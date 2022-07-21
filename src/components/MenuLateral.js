ee501fe4f0e7803a825b9c33178e67c4f81a597f
Lucas <lucas.silva@kardbank.com.br>
2022-07-20 23:53:48 -0300

    alteracaomenu

--- src/components/MenuLateral.js
+++ src/components/MenuLateral.js
@@ -1,29 +1,39 @@
 import React, { useState } from "react";
 import styles from '../styles/MenuLateral.module.scss';
+import { AiOutlineHome } from "react-icons/ai";
 
 const MenuLateral = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
- 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <>
-      <div    className={`${styles['menu-lateral']} ${
-              isMenuOpen ? styles['menu-lateral-open'] : ''
-            }`}>
-      <button
-            className={`${styles['toggler-button']} ${
-              isMenuOpen ? styles['is-menu-open'] : ''
-            }`}
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-            type="button"
-          >
-            <span className={styles.line} />
-            <span className={styles.line} />
-            <span className={styles.line} />
-          </button>
-         
+      <div
+        className={`${styles["menu-lateral"]} ${
+          isMenuOpen ? styles["menu-lateral-open"] : ""
+        }`}
+      >
+        <button
+          className={`${styles["toggler-button"]} ${
+            isMenuOpen ? styles["is-menu-open"] : ""
+          }`}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          type="button"
+        >
+          <span className={styles.line} />
+          <span className={styles.line} />
+          <span className={styles.line} />
+        </button>
+        <span
+          className={`${styles["div-menu"]} ${
+            isMenuOpen ? styles["div-menuOpen"] : ""
+          }`}
+        >
+          <a href="/home">
+            <AiOutlineHome size={30}></AiOutlineHome>
+            <h1>Home</h1>
+          </a>
+        </span>
       </div>
-      
-   
     </>
   );
 };
